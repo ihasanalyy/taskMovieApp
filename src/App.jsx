8c294e52e52a0644ff4d93a1a58af1f49f267000
Hassan Ali <ihasanalyy@gmail.com>
2024-12-10 20:21:19 +0500

    taskDone

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,47 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { SearchBar } from './Components/Search/SearchBar';
 import { Cards } from './Components/Cards/Cards';
+import { SideBars } from './Components/SideBars/SideBars';
 import axios from 'axios';
 
 function App() {
   const [search, setSearch] = useState('')
   const [movies, setMovies] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     callApi()
-  },[search])
-  
-  const callApi = async () =>{
-   try{
-    const response = await axios.get(`https://www.omdbapi.com/?s=${search}&apikey=2069fe59`);
-    console.log(response.data)
-    setMovies(response.data.Search)
-   }
-   catch (error){
-    console.log(error)
-   }
+  }, [search])
+
+  const callApi = async () => {
+    try {
+      const response = await axios.get(`https://www.omdbapi.com/?s=${search}&apikey=2069fe59`);
+      console.log(response.data)
+      setMovies(response.data.Search)
+    }
+    catch (error) {
+      console.log(error)
+    }
   }
- 
+
 
   return (
     <>
-  <SearchBar setSearch={setSearch} />
-<div >
-  {movies?.map((movie, key) => (
-    <Cards key={movie.imdbID} movie={movie} index={key} />
-  ))}
-</div>
+      <SearchBar setSearch={setSearch} />
+      <div className="flex">
+        {/* Sidebar */}
+        <SideBars className="w-1/4" />
 
+        {/* Main content for movies */}
+        <div className="bg-black flex flex-wrap gap-4 p-4 text-white w-full">
+          {movies?.map((movie, key) => (
+            <div className="w-full sm:w-1/2 lg:w-1/4 m-auto" key={movie.imdbID}>
+              <Cards movie={movie} index={key} />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
+
   )
 }
 
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -3,17 +3,17 @@ const Cards =({movie})=>{
 
     // console.log(movies.search)
     return(
-        <div class="bg-white flex justify-center items-center w-screen p-5 bg-black grid gap-4 p-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-  <div class="shadow-md max-w-2xl p-3 rounded-lg dark:bg-gray-600 dark:text-gray-200 mx-auto">
+        // <div class="bg-white flex justify-center items-center w-screen p-5 border-red-600">
+  <div class=" p-3 rounded-lg dark:bg-gray-600 dark:text-gray-200 m-auto w-100">
   <img 
-  className="bg-green-200 h-40 rounded-lg flex justify-center items-center text-gray-700" 
+  className="h-40 rounded-lg flex justify-center items-center w-100" 
   src={movie.Poster} 
   alt={movie.Title}
 />
     <h1 class="text-xl sm:text-2xl font-semibold p-3">{movie.Title}</h1>
     <p class="text-sm sm:text-md md:text-lg font-medium px-3">{movie.Year}</p>
   </div>
-</div>
+// </div>
     )
 }
 
